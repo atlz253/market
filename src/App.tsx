d9dc3f400fb5dbe6eaa77c3df3e29c9f94159309
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2024-03-15 12:53:27 +0300

    собрал базовый app shell VKUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  AppRoot,
+  Group,
+  Header,
+  Panel,
+  PanelHeader,
+  SimpleCell,
+  SplitCol,
+  SplitLayout,
+  View,
+  usePlatform,
+} from "@vkontakte/vkui";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const platform = usePlatform();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AppRoot>
+      <SplitLayout
+        header={platform !== "vkcom" && <PanelHeader delimiter="none" />}
+      >
+        <SplitCol autoSpaced>
+          <View activePanel="main">
+            <Panel id="main">
+              <PanelHeader>VKUI</PanelHeader>
+              <Group header={<Header mode="secondary">Items</Header>}>
+                <SimpleCell>Hello</SimpleCell>
+                <SimpleCell>World</SimpleCell>
+              </Group>
+            </Panel>
+          </View>
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
+  );
 }
 
-export default App
+export default App;
